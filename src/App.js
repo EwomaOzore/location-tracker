f8fe2a325fe3e2009cb74d02b1a381f6725ccc95
Ewoma Ozore <ewomaozore@gmail.com>
2024-02-25 09:00:41 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Map from './components/Map';
 import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className='app-container'>
-      <Nav />
-      <Map />
-    </div>
+    <Router>
+      <div className='app-container'>
+        <Nav />
+        <Routes>
+          <Route path='/map' component={Map} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClockRotateLeft, faGears, faMapLocationDot, faSliders } from '@fortawesome/free-solid-svg-icons';
+import logo from '../images/tracking.jpg';
+import profile from '../images/profile.jpg';
 import '../App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faClockRotateLeft, faGears, faSliders } from '@fortawesome/free-solid-svg-icons';
-import logo from '../images/tracking.jpg'
-import profile from '../images/profile.jpg'
 
 function Nav() {
     return (
@@ -12,21 +13,24 @@ function Nav() {
                 <img src={logo} alt='Logo' />
             </div>
             <div className='nav-button'>
-                <div className='btn'>
+                <Link to='/map' className='btn' title='Map'>
+                    <FontAwesomeIcon icon={faMapLocationDot} size='lg' />
+                </Link>
+                <Link to='/history' className='btn' title='History'>
                     <FontAwesomeIcon icon={faClockRotateLeft} size='lg' />
-                </div>
-                <div className='btn'>
+                </Link>
+                <Link to='/preferences' className='btn' title='Preferences'>
                     <FontAwesomeIcon icon={faSliders} size='lg' />
-                </div>
-                <div className='btn'>
+                </Link>
+                <Link to='/settings' className='btn' title='Settings'>
                     <FontAwesomeIcon icon={faGears} size='lg' />
-                </div>
+                </Link>
             </div>
             <div className='profile'>
                 <img src={profile} alt='Profile' />
             </div>
         </div>
     );
-};
+}
 
 export default Nav;
